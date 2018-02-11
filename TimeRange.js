61a214aabc674e7283e6d7d36d21fabc1a345d10
Peter Joseph <contact@peterjoseph.net>
2018-02-12 07:10:56 +0800

    Create empty react container

---
+++ TimeRange.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+export default class TimeRange extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
