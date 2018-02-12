c3b331691e26274fe31fef761e2b1b5d2227c17f
Peter Joseph <contact@peterjoseph.net>
2018-02-13 07:18:11 +0800

    Component rendering correctly with webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import moment from 'moment';
 
 export default class TimeRange extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <div />
+      <div>Time Range component</div>
     );
   }
 }
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {},
+        }
+      },
+      {
+        test: /.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /.(png|jpg|gif|eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      },
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
