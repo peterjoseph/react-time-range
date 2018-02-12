671ab0b85200b46190ee46f424efd0f54cbdce32
Peter Joseph <contact@peterjoseph.net>
2018-02-13 07:38:46 +0800

    Define basic default propTypes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-export default class TimeRange extends Component {
+class TimeRange extends Component {
+
+  constructor(props) {
+		super(props);
+	}
+ 
   render() {
+		const {
+      startMoment,
+      endMoment,
+      use24Hours,
+		} = this.props;
+
     return (
-      <div>Time Range component</div>
+      <div />
     );
   }
 }
+
+TimeRange.defaultProps = {
+  use24Hours: false
+};
+
+TimeRange.propTypes = {
+  startMoment: PropTypes.object,
+  endMoment: PropTypes.object,
+  use24Hours: PropTypes.bool,
+  hourIncrement: PropTypes.number,
+  minuteIncrement: PropTypes.number,
+  onClick: PropTypes.func,
+  onChange: PropTypes.func,
+};
+
+export default TimeRange;
