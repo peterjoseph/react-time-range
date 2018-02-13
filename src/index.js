585dae4d592e14c8a2a0ff472ea4138107aa2469
Peter Joseph <contact@peterjoseph.net>
2018-02-14 06:54:43 +0800

    Fixed incorrect time increment values

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class TimeRange extends Component {
 		super(props);
   }
   
-  generateTimeIncrement() {
+  generateTimeIncrement() { // Create an array of all possible times that can be selected
     const minuteIncrement = 60 / this.props.minuteIncrement;
     let timeArray = [];
     for (let i = 0; i < 24; i++) {
@@ -16,7 +16,7 @@ class TimeRange extends Component {
         const time = {
           HH: ("0" + (i)).slice(-2),
           MM: ("0" + (j * this.props.minuteIncrement)).slice(-2),
-          hh: (i == 0) ? "12" : (i >= 12 ? ("0" + ((i) - 12)).slice(-2) : ("0" + (i)).slice(-2)),
+          hh: (i == 0) ? "12" : (i == 12 ? "12" : (i > 12 ? ("0" + ((i) - 12)) : ("0" + (i)))).slice(-2),
           mm: ("0" + (j * this.props.minuteIncrement)).slice(-2),
           active: true,
           period: i >= 12 ? "PM" : "AM"
@@ -25,6 +25,22 @@ class TimeRange extends Component {
       }
     }
     return timeArray;
+  }
+
+  calculateRoundedTimeValue(mnt) { // If we receive a moment value, find nearest time increment
+
+  }
+
+  changeTime() { // Return new moment() object when time changes
+
+  }
+
+  validTimeCheck() { // Confirm if start and end times are valid ranges
+    
+  }
+
+  hideTimeValues() { // Generate list of disabled values for a time range
+
   }
  
   render() {
@@ -80,7 +96,7 @@ TimeRange.propTypes = {
   endLabel: PropTypes.string,
   startMoment: PropTypes.object,
   endMoment: PropTypes.object,
-  minuteIncrement: PropTypes.oneOf([10,15,20,30,60]),
+  minuteIncrement: PropTypes.oneOf([1,5,10,15,20,30,60]),
   className: PropTypes.string,
   onClick: PropTypes.func,
   onChange: PropTypes.func, // This should also return the duration
